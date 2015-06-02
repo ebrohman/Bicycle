0eafb88062b3cfea4fae416969403279f4459f71
ebrohman <ericbrohman@gmail.com>
2015-06-02 18:17:42 -0500

    change gear printout

--- Gear.rb
+++ Gear.rb
@@ -54,38 +54,52 @@ end
 
 
 wheel = Bicycle::Wheel.new()
-cassette = Bicycle::Cassette.new()
-cassette2 = Bicycle::Cassette.new([11,12,13,14,15,16,17,19,21,23,25])
+cassette28 = Bicycle::Cassette.new()
+cassette26 = Bicycle::Cassette.new([11,12,13,14,15,16,17,19,21,23,26])
+cassette25 = Bicycle::Cassette.new([11,12,13,14,15,16,17,19,21,23,25])
+chainring_mid = { big: 52, small: 36 }
+chainring_compact = { big: 50, small: 34 }
 
-regular = Bicycle::Gear.new(cassette: cassette, wheel: wheel)
+regular = Bicycle::Gear.new(cassette: cassette28, wheel: wheel)
+regular_25 = Bicycle::Gear.new(cassette: cassette25, wheel: wheel)
+regular_26 = Bicycle::Gear.new(cassette: cassette26, wheel: wheel)
 
-regular_25 = Bicycle::Gear.new(cassette: cassette2, wheel: wheel)
+mid_compact = Bicycle::Gear.new(cassette: cassette28, wheel: wheel, chainring: chainring_mid)
+mid_compact_25 = Bicycle::Gear.new(cassette: cassette25, wheel: wheel, chainring: chainring_mid)
+mid_compact_26 = Bicycle::Gear.new(cassette: cassette26, wheel: wheel, chainring: chainring_mid)
 
-mid_compact = Bicycle::Gear.new(cassette: cassette, wheel: wheel, chainring: {big:52, small:36})
+compact = Bicycle::Gear.new(cassette: cassette28, wheel: wheel, chainring: chainring_compact)
+compact_25 = Bicycle::Gear.new(cassette: cassette25, wheel: wheel, chainring: chainring_compact)
+compact_26 = Bicycle::Gear.new(cassette: cassette26, wheel: wheel, chainring: chainring_compact)
 
-mid_compact_25 = Bicycle::Gear.new(cassette: cassette2, wheel: wheel, chainring: {big:52, small:36})
-
-compact = Bicycle::Gear.new(cassette: cassette, wheel: wheel, chainring: {big:50, small:34})
-
-compact_25 = Bicycle::Gear.new(cassette: cassette2, wheel: wheel, chainring: {big:50, small:34})
-
-
+########
 puts
 puts "53/39 11-28"
-p regular.ratios.big, regular.ratios.small, regular.top_bottom
+p regular.ratios.big, regular.ratios.small, regular.jump_ratio
+puts
+puts "53/39 11-26"
+p regular_26.ratios.big, regular_26.ratios.small, regular_26.jump_ratio
 puts
 puts "53/39 11-25"
-p regular_25.ratios.big, regular_25.ratios.small, regular_25.top_bottom
+p regular_25.ratios.big, regular_25.ratios.small, regular_25.jump_ratio
 puts
+########
 puts "52/36 11-28"
-p mid_compact.ratios.big, mid_compact.ratios.small, mid_compact.top_bottom
+p mid_compact.ratios.big, mid_compact.ratios.small, mid_compact.jump_ratio
+puts
+puts "52/36 11-26"
+p mid_compact_26.ratios.big, mid_compact_26.ratios.small, mid_compact_26.jump_ratio
 puts
 puts "52/36 11-25"
-p mid_compact_25.ratios.big, mid_compact_25.ratios.small, mid_compact_25.top_bottom
+p mid_compact_25.ratios.big, mid_compact_25.ratios.small, mid_compact_25.jump_ratio
+########
 puts
 puts"50/34 11-28"
-p compact.ratios.big, compact.ratios.small, compact.top_bottom
+p compact.ratios.big, compact.ratios.small, compact.jump_ratio
+puts
+puts "50/34 11-26"
+p compact_26.ratios.big, compact_26.ratios.small, compact_26.jump_ratio
 puts
 puts "50/34 11-25"
-p compact_25.ratios.big, compact_25.ratios.small, compact_25.top_bottom
+p compact_25.ratios.big, compact_25.ratios.small, compact_25.jump_ratio
 puts
